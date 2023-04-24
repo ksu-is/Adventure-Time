4d7491b7d5e0db068de6cc6a713b8c1103d3a68a
ShinyaR444 <122241607+ShinyaR444@users.noreply.github.com>
2023-04-24 16:31:59 -0400

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,6 +1,6 @@
 name = input("Enter your name: ")
-print("Welcome " + name +  " The Game: Adventure Time!")
+print("Welcome " + name +  " The Game: Starbucks Adventure Time!")
 
-
+"Welcome", name, "to your Starbucks Adventure. You stayed up all night studying for your exam and need a boost for the day. Only problem is you start class in 30 minutes. Find your way to Starbucks and secure your drink before class starts!" 
 
 
