00d8ce7bc795baa1064e0654547e291220e2423f
ShinyaR444 <122241607+ShinyaR444@users.noreply.github.com>
2023-04-24 16:48:31 -0400

    Update Code.py
    Adding a print function to see the prompt and adding a new line function for easy reading.

--- Code.py
+++ Code.py
@@ -1,5 +1,5 @@
 name = input("Enter your name: ")
 print("Welcome " + name +  " The Game: Starbucks Adventure Time!")
-"Welcome", name, "to your Starbucks Adventure. You stayed up all night studying for your exam and need a boost for the day. Only problem is you start class in 30 minutes. Find your way to Starbucks and secure your drink before class starts!" 
+print("Welcome", name, "to your Starbucks Adventure. You stayed up all night studying for your exam and need a boost for the day. \n Only problem is you start class in 30 minutes. Find your way to Starbucks and secure your drink before class starts!")
 
 
