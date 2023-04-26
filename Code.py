33291c3c49e34040ed4bdb7c8327961eda019562
animdixo <122303330+animdixo@users.noreply.github.com>
2023-04-26 14:38:58 -0400

    Code.py
    trying to fix up the code to make everything clear.

--- Code.py
+++ Code.py
@@ -7,12 +7,11 @@ l2 = "talk"
 r2 = "ignore"
 
 name = input("Enter your name: ")
-print("Welcome " + name +  " The Game: Starbucks Adventure Time!")
 
 print("Welcome", name, "to your Starbucks Adventure. You stayed up all night studying for your exam and need a boost for the day. \n Only problem is you start class in 30 minutes. Find your way to Starbucks and secure your drink before class starts!")
 time = 30 
 while True:
-    q1 = input ("A tree fell. Where are you going to go? (left/right)")
+    q1 = input ("On your way to campus, a tree fell. To try to get around fall of the tree and potential traffic, you can go left or right? (left/right)")
     if q1.lower() == l1.lower():
         q2 = input("You get stopped by a professor. What will you do? (talk/ingore)")
     elif q1 == r2.lower(): 
