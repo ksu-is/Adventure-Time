9113acf64d833fa85cff5a3cd323bf66b2f46af3
animdixo <122303330+animdixo@users.noreply.github.com>
2023-04-26 14:59:53 -0400

    Code.py
    asking more options for the user to choose from when going right.

--- Code.py
+++ Code.py
@@ -11,9 +11,10 @@ name = input("Enter your name: ")
 print("Welcome", name, "to your Starbucks Adventure. You stayed up all night studying for your exam and need a boost for the day. \n Only problem is you start class in 30 minutes. Find your way to Starbucks and secure your drink before class starts!")
 time = 30 
 while True:
-    q1 = input ("On your way to campus, a tree fell. To try to get around fall of the tree and potential traffic, you can go left or right? (left/right)")
+    q1 = input ("On your way to campus, a tree fell. To try to get around the fall of the tree and potential traffic, you can go left or right? (pick one: left/right)")
     if q1.lower() == l1.lower():
-        q2 = input("You get stopped by a professor. What will you do? (talk/ingore)")
+        q2 = input("After that almost trafffic jam, you park your car at the Central Deck and begin your walk to Starbucks. On that walk, you see a friend that you haven't seen in while.\n This causes you guys to catch up and this goes on for about 5 minutes. You look at the time and notice you need to head to Starbuck soon so you're not late for class. Do you ask your friend to walk with you or just leave them behind? (pick one: walk/leave)")
+        q2 = input("You get stopped by a professor. You start catch up and seeing what's new. This goes on for about 5 minutes. You look at the time and notice you need to go to class NOW! What will you do? (pick one: talk/ingore)")
     elif q1 == r2.lower(): 
         print ( )
     else:
